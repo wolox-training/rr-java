dad5cc0e0914324279a1d9be36281d8cf0db3d5e
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2020-01-08 16:00:44 -0300

    Remove repository as it's for the next PR.

--- src/main/java/wolox/training/TrainingApplication.java
+++ src/main/java/wolox/training/TrainingApplication.java
@@ -3,9 +3,7 @@ package wolox.training;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("wolox.training.repositories")
 @EntityScan("wolox.training.models")
 @SpringBootApplication
 public class TrainingApplication {
--- src/main/java/wolox/training/repositories/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package wolox.training.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import wolox.training.models.Book;
-
-public interface BookRepository extends CrudRepository<Book, Long> {
-
-}
