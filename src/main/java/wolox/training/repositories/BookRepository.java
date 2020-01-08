0fe9e0e4772c32c8d2d6e2f2d6db7e409061cce8
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2020-01-08 15:33:39 -0300

    Add model and repository

---
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -0,0 +1,8 @@
+package wolox.training.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import wolox.training.models.Book;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+}
