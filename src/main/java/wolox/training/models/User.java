2b6fc87c78ea4fa0bb9f60c1ef1315f9efe01411
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2020-01-10 12:52:11 -0300

    Change generated value

--- src/main/java/wolox/training/models/User.java
+++ src/main/java/wolox/training/models/User.java
@@ -22,7 +22,7 @@ import wolox.training.exceptions.BookNotOwnedException;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(nullable = false)
