7e2ad4d202b73ce2ce3155402f129711b26cac9a
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2020-01-10 12:43:51 -0300

    Remove query annotation

--- src/main/java/wolox/training/repositories/UserRepository.java
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -1,12 +1,10 @@
 package wolox.training.repositories;
 
 import javax.validation.constraints.NotNull;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import wolox.training.models.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    @Query(value = "SELECT u FROM User u WHERE u.username = ?1")
     User findByUsername(@NotNull String username);
 }
