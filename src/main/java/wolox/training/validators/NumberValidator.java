9b0f92e008d38771f22b7560830badf36b11805f
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2020-01-10 13:36:18 -0300

    Fix number validator argument checks

--- src/main/java/wolox/training/validators/NumberValidator.java
+++ src/main/java/wolox/training/validators/NumberValidator.java
@@ -6,8 +6,8 @@ public class NumberValidator {
 
     public static void validate(int value, String name, int min, int max) {
         Preconditions
-            .checkArgument(value < min, String.format("%s can't be lower than %d", value, min));
+            .checkArgument(value >= min, String.format("%s can't be lower than %d", name, min));
         Preconditions
-            .checkArgument(value > max, String.format("%s can't be bigger than %d", value, max));
+            .checkArgument(value <= max, String.format("%s can't be bigger than %d", name, max));
     }
 }
