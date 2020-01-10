50454d1528e24d4c0a9be13975a8f8f1d95813f7
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2020-01-10 13:15:14 -0300

    Fix validator

--- src/main/java/wolox/training/models/User.java
+++ src/main/java/wolox/training/models/User.java
@@ -71,7 +71,7 @@ public class User {
     }
 
     public void setName(@NotNull String name) {
-        StringValidator.validate(username, "username", 120);
+        StringValidator.validate(username, "name", 120);
         this.name = name;
     }
 
