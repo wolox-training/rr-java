2334b0425689641d8c1621218172119205797043
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2020-01-10 13:14:07 -0300

    Fix validator

--- src/main/java/wolox/training/validators/StringValidator.java
+++ src/main/java/wolox/training/validators/StringValidator.java
@@ -12,7 +12,7 @@ public final class StringValidator {
 
     public static void validate(String value, String name, int maxLength) {
         Preconditions.checkNotNull(value, name + " can't be empty");
-        Preconditions.checkArgument(value.length() < 1, name + "can't be empty");
+        Preconditions.checkArgument(value.length() < 1, name + " can't be blank");
         Preconditions.checkArgument(value.length() > maxLength,
             String.format("%s can't be longer than %d characters.", name, maxLength));
     }
