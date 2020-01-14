222340ab13cb74462232d51a78c697897e8a293c
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2020-01-14 11:56:01 -0300

    Return optional instead of nulleable

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,10 +1,11 @@
 package wolox.training.repositories;
 
+import java.util.Optional;
 import javax.validation.constraints.NotNull;
 import org.springframework.data.repository.CrudRepository;
 import wolox.training.models.Book;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
 
-    Book findByAuthor(@NotNull String author);
+    Optional<Book> findByAuthor(@NotNull String author);
 }
