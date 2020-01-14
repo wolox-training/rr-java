2853a049cfd44c12c419c7e31aa06a758c799e4e
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2020-01-14 11:57:18 -0300

    Return optional instead of nulleable

--- src/main/java/wolox/training/repositories/UserRepository.java
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -1,10 +1,11 @@
 package wolox.training.repositories;
 
+import java.util.Optional;
 import javax.validation.constraints.NotNull;
 import org.springframework.data.repository.CrudRepository;
 import wolox.training.models.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    User findByUsername(@NotNull String username);
+    Optional<User> findByUsername(@NotNull String username);
 }
